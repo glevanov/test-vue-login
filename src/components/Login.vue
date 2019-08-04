5fcdf10dee8e6bdfd8e3378a97adffce80951b79
Grigoriy Levanov <glevanov@gmail.com>
2019-08-04 15:55:07 +0300

    Add naive login component

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Привет, %username%! Введи код для продолжения</p>
+    <label>
+      Логин
+      <input
+        v-model="login"
+        type="text"
+      >
+    </label>
+    <label>
+      Пароль
+      <input
+        v-model="password"
+        type="password"
+      >
+    </label>
+    <button type="button">
+      Запрос
+    </button>
+    <p>{{ login }}, {{ password }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      login: '',
+      password: '',
+    };
+  },
+};
+</script>
