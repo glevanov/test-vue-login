fab915f5e52c58ff484bbdc37254e1ee0ede9262
Grigoriy Levanov <glevanov@gmail.com>
2019-08-06 23:15:17 +0300

    Add ErrorMessage component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Ошибка: Нет такой комбинации имени пользователя и кода</p>
+    <router-link>
+      <Button
+        label="Вернуться в начало"
+      />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/Button.vue';
+
+export default {
+  name: 'ErrorMessage',
+  components: {
+    Button,
+  },
+};
+</script>
