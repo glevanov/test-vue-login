d77b9758a1e71bd5523094ec9f8c09c43ef6185a
Grigoriy Levanov <glevanov@gmail.com>
2019-08-06 20:39:55 +0300

    Add hardcoded axios request

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,21 +15,43 @@
         type="password"
       >
     </label>
-    <button type="button">
+    <button
+      type="button"
+      @click="getData"
+    >
       Запрос
     </button>
-    <p>{{ login }}, {{ password }}</p>
+    <p>Логин: {{ login }}, Пароль:{{ password }}</p>
+    <p>Ответ:<br>{{ response }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Login',
   data() {
     return {
       login: '',
       password: '',
+      URL: 'http://127.0.0.1:5000/check',
+      response: null,
     };
+  },
+  methods: {
+    getData() {
+      axios
+        .post(
+          this.URL, {
+            username: 'bob',
+            code: 'marley',
+          },
+        )
+        .then((response) => {
+          this.response = response.data;
+        });
+    },
   },
 };
 </script>
