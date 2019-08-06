90912f93632763b21c9dfa7675d92bc7659fe191
Grigoriy Levanov <glevanov@gmail.com>
2019-08-06 23:22:06 +0300

    Add content to Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,30 @@
 <template>
-  <p>sup</p>
+  <section class="dashboard">
+    <SubHeading
+      text="Личный кабинет"
+    />
+    <p>Интернет активен в течение {{ minutes }} минут</p>
+    <p>Доступно {{ traffic }} трафика для загрузки</p>
+  </section>
 </template>
 
 <script>
+import SubHeading from '@/components/SubHeading.vue';
+
 export default {
   name: 'Dashboard',
+  components: {
+    SubHeading,
+  },
+  props: {
+    traffic: {
+      type: String,
+      default: '',
+    },
+    minutes: {
+      type: String,
+      default: '',
+    },
+  },
 };
 </script>
-
-<style scoped>
-
-</style>
