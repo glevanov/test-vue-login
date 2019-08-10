be4cf13a5b0da4dd91e226b09c3d3ba968cb4112
Grigoriy Levanov <glevanov@gmail.com>
2019-08-10 21:27:17 +0300

    Add global styles & header styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
   <div id="app">
-    <header>
-      <h1>Кабинет пользователя</h1>
+    <header class="header">
+      <div class="wrapper">
+        <h1 class="header__heading">
+          Кабинет пользователя
+        </h1>
+      </div>
     </header>
     <main>
-      <router-view />
+      <div class="wrapper">
+        <router-view />
+      </div>
     </main>
   </div>
 </template>
+
+<style>
+  @import '~normalize.css';
+  @import './root.css';
+
+  #app {
+    font-family: -apple-system,
+    BlinkMacSystemFont,
+    Segoe UI, Roboto,
+    Oxygen, Ubuntu,
+    Cantarell, Fira Sans, Droid Sans,
+    Helvetica Neue,
+    sans-serif;
+  }
+
+  .wrapper {
+    max-width: 512px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .header {
+    background-color: var(--color-primary);
+  }
+
+  .header__heading {
+    margin: 0;
+
+    color: var(--color-text-contrast);
+  }
+</style>
