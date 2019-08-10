e0c878573f38dd4cbd27594321e9e0c5f7d53425
Grigoriy Levanov <glevanov@gmail.com>
2019-08-10 15:29:35 +0300

    Add prevent submit option to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="home">
-    <form @submit="handleSubmit">
+    <form @submit.prevent="handleSubmit">
       <SubHeading
         text="Введите имя пользователя чтобы продолжить"
       />
